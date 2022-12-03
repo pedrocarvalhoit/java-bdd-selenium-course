241f3c82a9bfdab7270b5896825503c21af8ecdc
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-12-03 13:57:04 +0000

    Initial commit

---
+++ src/main/java/com/selenium/seleniumcourse/SeleniumCourseApplication.java
@@ -0,0 +1,13 @@
+package com.selenium.seleniumcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumCourseApplication.class, args);
+	}
+
+}
